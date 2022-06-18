6b53ff75fea3fefe9879bbc77464d699f2658980
Solo Chevez <davidchevez90@gmail.com>
2022-06-17 20:23:22 -0600

    layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,13 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1 className='text-blue-600'>hello</h1>
+    <div className='h-screen w-screen flex flex-col items-center justify-center'>
+      <div className='text-white text-2xl text-center pb-3'>Which pokemon is rounder?</div>
+      <div className='border rounded p-8 flex justify-between items-center'>
+        <div className='w-16 h-16 bg-red-200' />
+        <div className='px-9'>vs</div>
+        <div className='w-16 h-16 bg-red-200' />
+      </div>
     </div>
   )
 }
