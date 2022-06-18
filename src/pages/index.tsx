e9d8c60b978af89f1d7e7e6fd8568198ad043a97
Solo Chevez <davidchevez90@gmail.com>
2022-06-17 21:16:41 -0600

    random pokemon helper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
+import { getOptionsForVote } from '@/utils/getRandomPokemon'
+import { trpc } from '@/utils/trpc'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
+  const [first, second] = getOptionsForVote()
+
   return (
     <div className='h-screen w-screen flex flex-col items-center justify-center'>
       <div className='text-white text-2xl text-center pb-3'>Which pokemon is rounder?</div>
       <div className='border rounded p-8 flex justify-between items-center'>
-        <div className='w-16 h-16 bg-red-200' />
+        <div className='w-16 h-16'>{first}</div>
         <div className='px-9'>vs</div>
-        <div className='w-16 h-16 bg-red-200' />
+        <div className='w-16 h-16'>{second}</div>
       </div>
     </div>
   )
