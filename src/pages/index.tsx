286a561326c19530428947a76df2a2e2abc31517
Solo Chevez <davidchevez90@gmail.com>
2022-06-17 22:33:32 -0600

    rounder button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { trpc } from '@/utils/trpc'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  const [ids, setIds] = useState(getOptionsForVote())
+  const [ids, updateIds] = useState(getOptionsForVote())
 
   const [first, second] = ids
 
@@ -13,6 +13,11 @@ const Home: NextPage = () => {
 
   if(firstPokemon.isLoading || secondPokemon.isLoading) return <div>Loading...</div>
 
+  const voteForRoundest = (selected: number) => {
+
+    updateIds(getOptionsForVote())
+  }
+
   return (
     <div className='h-screen w-screen flex flex-col items-center justify-center'>
       <div className='text-white text-2xl text-center pb-3'>Which pokemon is rounder?</div>
@@ -20,11 +25,13 @@ const Home: NextPage = () => {
         <div className='w-64 h-64 flex flex-col'>
           <img className='w-full' src={firstPokemon.data?.sprites.front_default ?? ''} />
           <div className='text-white text-center capitalize mt-[-2rem]'>{firstPokemon.data?.name}</div>
+          <button className='bg-white rounded-full text-black py-1 w-2/3 self-center' onClick={() => voteForRoundest(first)}>Rounder</button>
         </div>
         <div className='px-9 text-white'>VS</div>
-        <div className='w-64 h-64'>
+        <div className='w-64 h-64 flex flex-col'>
           <img className='w-full' src={secondPokemon.data?.sprites.front_default ?? ''} />
           <div className='text-white text-center capitalize mt-[-2rem]'>{secondPokemon.data?.name}</div>
+          <button className='bg-white rounded-full text-black py-1 w-2/3 self-center' onClick={() => voteForRoundest(second)}>Rounder</button>
         </div>
         <div className='p-2' />
       </div>
