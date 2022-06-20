39d2c6b16e41eac22b62701f01d7a4c39697d7da
Solo Chevez <davidchevez90@gmail.com>
2022-06-20 17:47:48 -0600

    fix trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,15 +8,20 @@ function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
+function getBaseUrl() {
+  if(typeof window === 'undefined') return "";
+  if(process.env.NEXT_PUBLIC_VERCEL_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+
+  return `http://localhost:${process.env.PORT ?? 3000}`
+}
+
 export default withTRPC<AppRouter>({
   config({ ctx }) {
     /**
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trpc';
+    const url = `${getBaseUrl()}/api/trpc`
 
     return {
       url,
